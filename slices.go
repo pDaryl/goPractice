256f23405a681b9a9a79c32d59ba632835d172d5
Daryl Peterson <pdaryl8@gmail.com>
2025-01-14 11:44:19 -0600

    created function rotateSlice

--- slices.go
+++ slices.go
@@ -2,31 +2,28 @@ package main
 
 import "fmt"
 
-func findIntersectingElements (slice1 []int, slice2 []int) []int {
-	if len(slice1) == 0 || len(slice2) == 0 {
-		return []int {}
+func rotateSlice ( slice []int , k int) []int {
+
+	if len(slice) <= 1 || k == 0 {
+		return slice
 	}
 
-	var result []int
-	freqCount := make(map[int] int)
-
-	for _, num := range slice1 {
-		freqCount[num]++
-	}
-		
-	for _, num := range slice2 {
-			if freqCount[num] > 0 {
-				result = append(result, num)
-				freqCount[num]--
-			}
+	steps := k % len(slice)
+	if steps == 0 {
+		return slice
 	}
 
-return result
+	part1 := slice[len(slice)-steps:]
+	part2 := slice[:len(slice)-steps]
+
+	result := append(part1, part2...)
+
+	return result
 }
 
 func main (){
-	slice1 := []int{1, 2, 2, 3, 4}
-	slice2 := []int{2, 2, 3, 5}
+	slice := []int{1, 2, 3, 4, 5}
+	k := 2
 
-	fmt.Println(findIntersectingElements(slice1, slice2));
+	fmt.Println(rotateSlice(slice, k))
 }
