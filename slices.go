aaa1820366a5087a71d606e9974475da232f1112
Daryl Peterson <pdaryl8@gmail.com>
2025-01-14 11:07:19 -0600

    created findIntersectingElements function

--- slices.go
+++ slices.go
@@ -2,26 +2,31 @@ package main
 
 import "fmt"
 
-func countFrequencies (slice []int) map[int]int{
-	if len(slice) == 0 {
-		return make(map[int]int)}
+func findIntersectingElements (slice1 []int, slice2 []int) []int {
+	if len(slice1) == 0 || len(slice2) == 0 {
+		return []int {}
+	}
 
+	var result []int
 	freqCount := make(map[int] int)
 
-	for _, num := range slice {
-
-		_, ok := freqCount[num]
-		if !ok {
-		freqCount[num] = 1
-		} else {
-			freqCount[num]++
-		}
+	for _, num := range slice1 {
+		freqCount[num]++
 	}
-	return freqCount
+		
+	for _, num := range slice2 {
+			if freqCount[num] > 0 {
+				result = append(result, num)
+				freqCount[num]--
+			}
+	}
+
+return result
 }
 
 func main (){
-slice := []int{1, 2, 3, 2, 1, 4, 2}
+	slice1 := []int{1, 2, 2, 3, 4}
+	slice2 := []int{2, 2, 3, 5}
 
-fmt.Println(countFrequencies(slice))
+	fmt.Println(findIntersectingElements(slice1, slice2));
 }
