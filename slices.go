93efafda03055c06447df60f8126aaba44dc1af0
Daryl Peterson <pdaryl8@gmail.com>
2025-01-19 10:07:03 -0600

    created removeDuplicateElements func

--- slices.go
+++ slices.go
@@ -1,73 +1,32 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-<<<<<<< HEAD
-func mergeSlices(slice1 []int, slice2 []int) []int {
-	if len(slice1) == 0 && len(slice2) == 0 {
-		return slice1
+func removeDuplicateElements(slice []int) []int {
+	if len(slice) == 0 {
+		return slice 
 	}
 
-	storeNums := make(map[int] bool)
-	var merged []int
+	var uniqueSL []int
+	uniqueNum := make(map[int] bool)
 
-	for _, num := range slice1 {
-	if !storeNums[num]{
-		storeNums[num] = true
-		merged = append(merged, num)
-	}
-	}
-
-	for _, num := range slice2 {
-		if !storeNums[num] {
-			storeNums[num] = true
-			merged = append(merged, num)
+	for _, num := range slice {
+		if !uniqueNum[num] {
+			uniqueNum[num] = true
+			uniqueSL = append(uniqueSL, num)
 		}
 	}
-
-return merged
-	
-}
-
-
-func main() {
-	slice1 := []int{1, 2, 3, 4}
-	slice2 := []int{3, 4, 5, 6}
-  
-	fmt.Println(mergeSlices(slice1, slice2))
-
-	//[]int{1, 2, 3, 4, 5, 6}
-  
-} 
-
-=======
-func leftOrRightSliceRotation(slice []int, k int, direction string) []int {
-	n := len(slice)
-
-	if n == 0 || k == 0{
-		return slice
-	}
-
-	k = k % n
-	if k == 0{
-		return slice
-	}
-
-	if direction == "right" {
-		return append(slice[n - k:], slice[:n - k]...)
-	} else if direction == "left" {
-		return append(slice[k:], slice[:k]...)
-	}
-return slice
+	return uniqueSL
 }
 
 
 func main(){
-slice := []int {1, 2, 3, 4, 5, 6}
-k := 2
+	slice1 := []int {1, 1, 1, 1, 1}
+	slice2 := []int {}
+	slice3 := []int {1, 1, 1, 2, 2, 3, 4, 2, 3, 3, 4, 4}
 
-fmt.Println(leftOrRightSliceRotation(slice, k, "right"))
+	fmt.Println(removeDuplicateElements(slice1))
+	fmt.Println(removeDuplicateElements(slice2))
+	fmt.Println(removeDuplicateElements(slice3))
+	
 }
->>>>>>> Function
