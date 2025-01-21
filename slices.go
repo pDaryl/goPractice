a2271f3995c48f91b9fec3fa58f0672be4e64d90
Daryl Peterson <pdaryl8@gmail.com>
2025-01-21 09:43:19 -0600

    created rotateSliceInPlace func

--- slices.go
+++ slices.go
@@ -2,97 +2,56 @@ package main
 
 import "fmt"
 
-func findMinAndMaxOfSubSlices (slice []int, k int) ([]int, []int) {
-	var minValues []int
-	var maxValues []int
-	
-	if len(slice) == 0 || k == 0{
-		return minValues, maxValues
-	}
 
-	if k > len(slice) {
-		min, max := minAndMax(slice)
-		maxValues = append(maxValues, max)
-		minValues = append(minValues, min)
-		return minValues, maxValues
+func reverse(slice []int, start int, end int) {
+	for start < end {
+		slice[start], slice[end] = slice[end], slice[start]
+		start++
+		end--
 	}
-
-	
-	
-	for start := 0; start <= len(slice)-k; start++{
-		subSlice := slice[start:start + k]
-		min, max := minAndMax(subSlice)
-		maxValues = append(maxValues, max)
-		minValues = append(minValues, min)
-	}
-
-	return minValues, maxValues
 }
 
+func rotateSliceInPlace(slice []int, k int) []int {
+	len := len(slice)
 
-func minAndMax (slice []int) (int, int) {
-	if len(slice) == 0 {
-		return 0, 0
+	if len == 0 || k == 0 {
+		return slice
 	}
 
-	min := slice[0]
-	max := slice[0]
-
-	for _, num := range slice {
-		if num < min {
-			min = num
-		}
-
-		if num > max {
-			max = num
-		}
-
+	k = k % len
+	
+	if k == 0 {
+		return slice
 	}
-	return min, max
+
+	reverse(slice, 0, len - 1) // reverse entire slice
+	reverse(slice, 0, k-1) // get the first elements
+	reverse(slice, k, len-1) // get the remaining elements
+
+	return slice
 }
-
 
 func main(){
-slice := []int {1, 3, 5, 7, 0, 6, 7, 8}
+slice := []int {1, 2, 3, 4, 5}
 k := 2
+fmt.Println(reverseSliceInPlace(slice, k))
 
-minValues, maxValues := findMinAndMaxOfSubSlices(slice, k)
+// -------------------------------
 
-fmt.Println("Minimum values:", minValues)
-fmt.Println("Maximum values:", maxValues)
+slice1 := []int {1, 2, 3, 4, 5}
+k1 := 0
+fmt.Println(reverseSliceInPlace(slice1, k1))
 
+// -----------------------------
 
-//########################################
+slice2 := []int {}
+k2 := 2
+fmt.Println(reverseSliceInPlace(slice2, k2))
 
+// ----------------------------
 
-slice1 := []int {}
-k1 := 2
-
-minValues1, maxValues1 := findMinAndMaxOfSubSlices(slice1, k1)
-
-fmt.Println("Minimum values:", minValues1)
-fmt.Println("Maximum values:", maxValues1)
-
-//########################################
-
-
-slice2 := []int {1, 3, 5, 7, 0, 6, 7, 8}
-k2 := 0
-
-minValues2, maxValues2 := findMinAndMaxOfSubSlices(slice2, k2)
-
-fmt.Println("Minimum values:", minValues2)
-fmt.Println("Maximum values:", maxValues2)
-
-//########################################
-
-
-slice3 := []int {1, 3, 5, 7, 0, 6, 7, 8}
-k3 := 15
-
-minValues3, maxValues3 := findMinAndMaxOfSubSlices(slice3, k3)
-
-fmt.Println("Minimum values:", minValues3)
-fmt.Println("Maximum values:", maxValues3)
+slice3 := []int {1, 2, 3, 4, 5}
+k3 := 7
+fmt.Println(reverseSliceInPlace(slice3, k3))
 
 }
