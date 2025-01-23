ba131821bdbcd44b41e688fff922b35e9fcd576a
Daryl Peterson <pdaryl8@gmail.com>
2025-01-23 09:28:08 -0600

    func in js and go findLengthOfLongestSubArrayOfKElements

--- practice.js
+++ practice.js
@@ -1,30 +1,39 @@
-function lengthLongestSubArrayOfSum (array, k) {
-  if (array.length === 0) return array;
+function findLengthOfLongestSubArrayOfKElements(array, k){
+if (array.length === 0 || k > array.length) return array.length;
 
-  if (k === 0) return "target sum is 0";
+if (k === 0) return "number of distinct elements is 0", k;
 
-  var maxLength = 0;
-  var currentSum = 0;
-  var start = 0;
 
-  for (let end = 0; end < array.length; end++){
-    currentSum += array[end];
+var maxLength = 0;
+var distinctNums = new Map();
+var start = 0;
 
-    if (currentSum > k && start <= end){
-      currentSum -= array[start];
-      start++;
-    }
-    if (array.slice(start, end+1).length > maxLength){
-      maxLength = array.slice(start, end+1).length;
-    }
+for (let end = 0; end < array.length; end++){
+  var currentNum = array[end];
+  if (!distinctNums.has(currentNum)){
+    distinctNums.set(currentNum, 1)
+  } else{
+    distinctNums.set(currentNum, distinctNums.get(currentNum) +1); 
   }
+
+  while(distinctNums.size > k){
+    distinctNums.set(array[start], distinctNums.get(array[start]) - 1);
+    if(distinctNums.get(array[start]) === 0){
+      distinctNums.delete(array[start]);
+    }
+    start++;
+  }
+if (end - start + 1 > maxLength){
+  maxLength = end - start + 1;
+}
+}
 return maxLength;
 }
 
-const array = [2, 1, 5, 2, 3, 2]
-const k = 5
 
-console.log(lengthLongestSubArrayOfSum(array, k));
+const array = [1, 1, 1, 2, 2, 2, 3, 4, 5, 6];
+const k = 3;
 
+console.log(findLengthOfLongestSubArrayOfKElements(array, k));
 
 
--- slices.go
+++ slices.go
@@ -2,33 +2,41 @@ package main
 
 import "fmt"
 
-func lengthLongestSubSliceOfSum (slice []int, k int) (int) {
-	if len(slice) == 0 {
-		return 0
-	}
-
-	maxLength := 0
-	currentSum := 0
-	start := 0
-
-	for end := 0; end < len(slice); end++{
-		currentSum += slice[end]
-		
-		if currentSum > k && start <= end{		
-			currentSum -= slice[start]
-			start++
-		}
-		
-		if len(slice[start:end+1]) > maxLength{
-			maxLength = len(slice[start:end+1])
-		}
-	}
-		return maxLength
+func  findLengthLongestSubSliceKElem(slice []int, k int) int {
+if len(slice) == 0 || k > len(slice) {
+	return len(slice)
 }
 
-func main(){
-slice := []int{2, 1, 5, 2, 3, 2}
-k := 7
+maxLength := 0
+distinctKeys := make(map[int] int)
+start := 0
 
-fmt.Println(lengthLongestSubSliceOfSum(slice, k))
+for end := 0; end < len(slice); end++ {
+	_, ok := distinctKeys[slice[end]]
+	if !ok {
+		distinctKeys[slice[end]] = 1
+	}else {
+		distinctKeys[slice[end]]++
+	}
+
+	for len(distinctKeys) > k {
+		distinctKeys[slice[start]]--
+		if distinctKeys[slice[start]] == 0 {
+			delete(distinctKeys, slice[start])
+		}
+		start++
+	}
+	if end - start + 1 > maxLength {
+		maxLength = end - start + 1
+	}
+}
+return maxLength
+}
+
+
+func main(){
+slice := []int{1, 1, 1, 2, 2, 2, 3, 4, 5, 6}
+k := 3
+
+fmt.Println(findLengthLongestSubSliceKElem(slice, k))
 }
