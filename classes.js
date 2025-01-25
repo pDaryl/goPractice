fbda1a6357b4cb5dbdb20478fd952b06705d587b
Daryl Peterson <pdaryl8@gmail.com>
2025-01-25 12:16:05 -0600

    created library class and struct

--- classes.js
+++ classes.js
@@ -1,28 +1,85 @@
 class Author {
-    constructor(name, nationality) {
-        this.name = name;
+    constructor(authorName, nationality) {
+        this.authorName = authorName;
         this.nationality = nationality;
     }
 }
 
-class Books {
-    constructor(title, pages, author) {
-        this.title = title;
-        this.pages = pages;
-        this.author = author;
+class Genre {
+    constructor(genreName) {
+        this.genreName = genreName;
     }
 }
 
-function createBook(title, pages, name, nationality){
-    const author = new Author(name, nationality);
-    const book = new Books(title, pages, author);
+class Book {
+    constructor(title, pages, author, genre) {
+        this.title = title;
+        this.pages = pages;
+        this.author = author;
+        this.genre = genre;
+    }
+}
+
+class Library {
+    constructor(libraryName, location) {
+        this.libraryName = libraryName;
+        this.location = location;
+        this.books = [];
+        this.genres = [];
+    }
+
+
+addBook(book){
+this.books.push(book);
+}
+
+addGenre(genre){
+    this.genres.push(genre);
+}
+}
+
+function createAuthor(authorName, nationality){
+    const author = new Author(authorName, nationality);
+    return author;
+}
+
+function createGenre(genreName){
+    const genre = new Genre(genreName);
+    return genre;
+}
+
+function createBook(title, pages, author, genre) {
+    const book = new Book(title, pages, author, genre);
     return book;
 }
 
-function printBook(book){
-    console.log(`Book(Title: ${book.title}, Pages: ${book.pages}, Author: [Name: ${book.author.name}, Nationality: ${book.author.nationality}])`);
+function createLibrary(libraryName, location){
+    const library = new Library(libraryName, location);
+    return library;
 }
 
 
-const b = createBook("Daryl's World", 369, "Daryl Peterson", "American");
-printBook(b);
+const author1 = createAuthor("Daryl", "American");
+const author2 = createAuthor("Peterson", "American");
+
+const genre1 = createGenre("Comedy");
+const genre2 = createGenre("fantasy");
+
+const book1 = createBook("Daryl's World of Excellence", 600, author1, genre1);
+const book2 = createBook("Peterson's Highway Expedition", 400, author2, genre2);
+
+const library = createLibrary("New Orlean's Public Library", "New Orleans");
+
+library.addBook(book1);
+library.addBook(book2);
+library.addGenre(genre1);
+library.addGenre(genre2);
+
+console.log(`${library.libraryName}, ${library.location}`);
+console.log(`Books in the Library:`);
+for (const b of library.books){
+console.log(`- ${b.title} by ${b.author.authorName}`);
+}
+for (const g of library.genres){
+    console.log(`- ${g.genreName}`);
+}
--- structs.go
+++ structs.go
@@ -6,37 +6,96 @@ import (
 
 
 type Author struct{
-	Name string
+	Name string 
 	Nationality string
+}
+
+type Genre struct{
+	Name string
 }
 
 type Book struct{
 	Title string
 	Pages int
 	Author Author 
+	Genre Genre
 }
 
-func createBook (title string, pages int, name string, nationality string) Book {
+type Library struct{
+	Name string
+	Location string
+	Book []Book
+	Genre []Genre
+}
+
+func createAuthor(name string, nationality string) Author {
+	author := Author{
+		Name:        name,
+		Nationality: nationality,
+	}
+	return author
+}
+
+func createGenre(name string) Genre {
+	genre := Genre{
+		Name: name,
+	}
+	return genre
+}
+
+func createBook(title string, pages int, author Author, genre Genre) Book {
 	book := Book{
 		Title:  title,
 		Pages:  pages,
-		Author: Author{
-			Name:        name,
-			Nationality: nationality,
-		},
+		Author: author,
+		Genre:  genre,
 	}
 	return book
 }
 
-func printBook(book Book){
-	fmt.Printf("Book(Title: %s, Pages: %d, Author: [Name: %s, Nationality: %s])\n", 
-book.Title, 
-book.Pages, 
-book.Author.Name,
-book.Author.Nationality)
+func createLibrary(name string, location string) Library{
+	library := Library{
+		Name:     name,
+		Location: location,
+		Book:     []Book{},
+		Genre:    []Genre{},
+	}
+	return library
+}
+
+func (l *Library) addBook(book Book){ // what is going on in here? i know i am passing a pointer to library but why?
+	l.Book = append(l.Book, book) // why am i appending the book here?
+}
+
+func (l *Library) addGenre(genre Genre){ // what is going on here? i know i am passing a pointer to library but why? 
+	l.Genre = append(l.Genre, genre) // why am i appending the genre here?
 }
 
 func main(){
-b := createBook("Daryl's World", 369, "Daryl Peterson", "American")
-printBook(b)
+author1 := createAuthor("daryl", "USA")
+author2 := createAuthor("peterson", "USA")
+
+genre1 := createGenre("Fantasy")
+genre2 := createGenre("Action")
+
+book1 := createBook("D's World", 306, author1, genre1)
+book2 := createBook("P's World", 406, author2, genre2)
+
+library := createLibrary("New Orlean's Public Library", "New Orleans")
+
+library.addBook(book1)
+library.addBook(book2)
+library.addGenre(genre1)
+library.addGenre(genre2)
+
+fmt.Printf("Library(Name: %s, Location: %s)\n", library.Name, library.Location)
+fmt.Println("Books in Library:")
+for _, books := range library.Book{
+	fmt.Printf("- %s by %s\n", books.Title, books.Author.Name)
+}
+fmt.Println("Genre's in Library:")
+for _, genre := range library.Genre {
+	fmt.Printf("- %s\n", genre.Name)
+}
+
 }
